7a0805913a43d51bbd39f9d8eeaf88902d7050f7
AleksandrZhuravel <cah40c@tut.by>
2020-08-28 17:49:18 +0300

    Initial commit

---
+++ src/test/java/tests/YandexTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
+public class YandexTests {
+    @Test
+    void yandexLocationPageShouldOpen() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.headless = true;
+
+        open("https://yandex.ru");
+
+        $(".geolink__reg").click();
+        $(".input__control").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $(".input__control").setValue("Нячанг").pressEnter().pressEnter();
+
+        $(".geolink__reg").shouldHave(text("Нячанг"));
+    }
+}
